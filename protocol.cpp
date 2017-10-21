fec158feea295cba219d658249fdd01ac2d53a8e
siddhant-varma <siddhantvarma@outlook.com>
2017-10-21 17:39:00 +0530

    Perfectlly working

--- protocol.cpp
+++ protocol.cpp
@@ -2,14 +2,125 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <windows.h>
+
+using namespace std;
 
 int randNum(float chance){
-	srand(0);
-	int temp = rand();
-	/*if(temp <= chance * 100){
-		return temp;
+	//srand(0);
+	int temp = rand() % 100;
+	cout<<"\n\t\t\tRandom Num = "<<temp;
+	if(temp <= chance * 100){
+		return 1;
 	}
-	else return 0;*/
-	return temp;
+	else return 0;
+	//return temp;
 }
 
+packet GetData(void){
+	packet p;
+	cout<<"\nEnter Message:\t";
+	cin>>p.data;
+	return p;
+}
+
+frame MakeFrame(seqNo n, packet p){
+	cout<<"\nMaking Frame...";
+	frame f;
+	f.seq = n;
+	f.info = p;
+	return f;
+}
+
+frame StoreFrame(frame f){
+	cout<<"\nStoring Frame...";
+	return f;
+}
+
+bool SendFrame(frame &f){
+	cout<<"\nSending Frame...";
+	/*return ToPhysicalLayer(f);
+}
+
+bool ToPhysicalLayer(frame &f){*/
+	return OverTransmissionMedia(f);
+}
+
+bool OverTransmissionMedia(frame &f){
+	if(randNum(.9)){
+		f.ack = (f.seq + 1) % 2;//for receiver side SendFrame()
+		return true;
+	}
+	else{
+		cout<<"\nFrame Lost during transmission...";
+		return false;
+	}
+}
+
+/*void WaitForEvent(Event &e){
+
+}*/
+seqNo ReceiveFrame(frame &f){
+	cout<<"\nReceving Acknowledgement...";
+	return f.ack;
+}
+
+void PurgeFrame(seqNo s, frame &f){
+	if(f.seq == s){
+		f.seq =  -1;
+		cout<<"\nFrame Purged";
+	}
+	return;
+} 
+
+//static int MAX_TIME = 100;
+//static int running = 0;
+Event StartTimer(){
+	cout<<"\nTimer Running...";
+	for( ; running < MAX_TIME; running++){
+		Sleep(1);
+		if(randNum(.8))
+			return FRAME_ARRIVED;
+	}
+	cout<<"\nTimer TimeOut";
+	return TIMEOUT;
+}
+
+void StopTimer(){
+	running = 0;
+	cout<<"\nTimer stopped...";
+	return;
+}
+
+//ReceiverSide
+
+frame& ReceiverFrame(frame &fr){
+	cout<<"\n\t\t\t\t\t\tIn ReceiveFrame()..."<<fr.seq;
+	return fr;
+}
+
+packet ExtractData(frame &fr){
+	cout<<"\nExtracting Frame...";
+	return fr.info;
+}
+
+void DeliverData(packet &p){
+	cout<<"\nMessage:\t"<<p.data;
+	cout<<"\nPacket successfully delivered to NL.";
+	return;
+}
+
+bool corrupted(frame &fr, int a){
+	if(a)
+		cout<<"\nAcknowledgement ";
+	else
+		cout<<"\nFrame ";
+	if(randNum(.9)){
+		cout<<"Not Corrupted.\n";
+		return false;
+	}
+	else{
+		cout<<"Corrupted...\n";
+		return true;
+	}
+}
--- protocol.h
+++ protocol.h
@@ -1,6 +1,55 @@
 #ifndef PROTOCOL
 #define PROTOCOL
 
-int randNum(float);
+typedef int seqNo;	//Sequence Number
 
+typedef enum{
+	PACKET_AVAILABLE,	//Triggers when NL sends packet
+	FRAME_ARRIVED,
+	ERROR,
+	TIMEOUT
+} Event;
+
+typedef struct{
+	char data;		//Data contained in and as packet
+} packet;
+
+typedef struct{
+	seqNo seq;
+	seqNo ack;		//Acknowledge Number
+	packet info;
+} frame;			//Frame structure comprising packet strucutre
+
+int randNum(float);	//Generates Random number and returns accordingly boolean with chance passed as parameter
+
+packet GetData(void);
+
+frame MakeFrame(seqNo, packet);
+
+frame StoreFrame(frame);
+
+bool SendFrame(frame&);
+
+//void ToPhysicalLayer(frame&);
+
+bool OverTransmissionMedia(frame&);
+
+seqNo ReceiveFrame(frame&);
+
+void PurgeFrame(seqNo, frame&);
+
+static int MAX_TIME = 100;
+static int running = 0;
+Event StartTimer();
+void StopTimer();
+
+//ReceiverSide
+
+frame& ReceiverFrame(frame&);
+
+packet ExtractData(frame&);
+
+void DeliverData(packet&);
+
+bool corrupted(frame&, int);
 #endif
