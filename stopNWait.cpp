f0b5003475b7230690cc88f44200f77c828599a2
siddhant-varma <siddhantvarma@outlook.com>
2017-10-21 11:00:01 +0530

    Basic algo
    same as provided in book
    Forozuan 4th Ed, page 320

--- stopNWait.cpp
+++ stopNWait.cpp
@@ -5,5 +5,35 @@ using namespace std;
 
 int main(void){
 	
+	int Sn = 0;
+	bool canSend = true;
+	
+	while(true){
+		if(Event (RequestToSend) && canSend){
+			GetData();
+			MakeFrame(Sn);
+			StoreFrame(Sn);
+			SendFrame(Sn);
+			StartTimer();
+			Sn++;
+			canSend = false;
+		}
+		
+		WaitForEvent();
+		if(Event (ArrivalNotification) ){
+			ReceiveFrame(ackNo);
+			if( ! corrupted && ackNo == Sn){
+				StopTime();
+				PurgeFrame(Sn-1);
+				canSend = true;
+			}
+		}
+		
+		if(Event(TimeOut)){
+			StarTimer();
+			ResendFrame(Sn - 1);
+		}
+	}
+	
 	return 0;
 }
