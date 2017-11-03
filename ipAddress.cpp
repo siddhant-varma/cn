25aba679c70232b80b783bc2ce7388602552dea7
siddhant-varma <siddhantvarma@outlook.com>
2017-11-03 11:04:12 +0530

    Redundant code removed

--- ipAddress.cpp
+++ ipAddress.cpp
@@ -9,12 +9,7 @@ int main(void){
 	
 	string::size_type pos;
 	
-	//pos = address.find(".");
-	//string tar = address.substr(0, address.find("."));
-	//cout<<tar;
-	
 	int num = stoi(address.substr(0, address.find(".")), &pos);
-	//cout<<num;
 	
 	if(num < 128)
 		cout<<"\Class A";
@@ -28,19 +23,5 @@ int main(void){
 		cout<<"\Class E";
 	else
 		cout<<"\nWrong IP Address...";
-	
-	/*
-	switch(num){
-		case :	
-			break;
-		case num < 192:	
-			break;
-		case num < 224:	cout<<"\Class C";
-			break;
-		case num < 240:	cout<<"\Class D";
-			break;
-		case num < 256:	cout<<"\Class E";
-			break;
-	}*/
 	
 }
