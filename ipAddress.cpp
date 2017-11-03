dd5854b5d5708a67de8cd9627df7fa50b56e4897
siddhant-varma <siddhantvarma@outlook.com>
2017-11-03 11:59:25 +0530

    Error detectiong for wrong address added

--- ipAddress.cpp
+++ ipAddress.cpp
@@ -7,25 +7,44 @@ int main(void){
 	cout<<"Enter IP Address:\t";
 	cin>>address;
 	
-	string::size_type pos;
+	string::size_type pos = 0, prev = -1, sz;
+	int add[4];
 	
-	//pos = address.find(".");
-	//string tar = address.substr(0, address.find("."));
-	//cout<<tar;
+	int i = 0;
+	while(pos != string::npos){
+		pos = address.find(".", prev+1);
+		//cout<<"\tpos = "<<pos<<endl;
+		string tar = address.substr(prev + 1, pos - prev - 1);
+		//cout<<tar<<endl;
+		prev = pos;
+		add[i++] = stoi(tar, &sz);
+	}
+		
+	/*pos = address.find(".");
+	string tar = address.substr(0, address.find("."));
+	cout<<tar;*/
 	
-	int num = stoi(address.substr(0, address.find(".")), &pos);
+	//int num = stoi(address.substr(0, address.find(".")), &pos);
 	//cout<<num;
+	bool valid = true;
+	for(int j = 0; j < 4; j++){
+		if(add[j] > 255 || add[j] < 0)
+			valid = false;
+	}
 	
-	if(num < 128)
-		cout<<"\Class A";
-	else if(num < 192)
-		cout<<"\Class B";
-	else if(num < 224)
-		cout<<"\Class C";
-	else if(num < 240)
-		cout<<"\Class D";
-	else if(num < 256)
-		cout<<"\Class E";
+	if(valid){
+		int num = add[0];
+		if(num < 128)
+			cout<<"\Class A";
+		else if(num < 192)
+			cout<<"\Class B";
+		else if(num < 224)
+			cout<<"\Class C";
+		else if(num < 240)
+			cout<<"\Class D";
+		else
+			cout<<"\Class E";
+	}
 	else
 		cout<<"\nWrong IP Address...";
 	
